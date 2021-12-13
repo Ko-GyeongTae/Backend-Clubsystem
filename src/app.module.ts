5062e689997faca6e07937a849f1964d7c455269
Ko-GyeongTae <kokt0203@naver.com>
2021-12-13 19:20:21 +0900

    ADD - Basic Structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { ClubModule } from './club/club.module';
+import { NoticeModule } from './notice/notice.module';
+import { VoteModule } from './vote/vote.module';
+import { LetterModule } from './letter/letter.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, ClubModule, NoticeModule, VoteModule, LetterModule],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
