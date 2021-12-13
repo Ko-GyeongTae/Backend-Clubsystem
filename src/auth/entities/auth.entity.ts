67faff23edebd0bd9f0c569f69ea042ba9c076ff
Ko-GyeongTae <kokt0203@naver.com>
2021-12-13 20:08:10 +0900

    ADD - auth entity

---
+++ src/auth/entities/auth.entity.ts
@@ -0,0 +1,34 @@
+import { Club } from "src/club/entities/club.entity";
+import { BaseEntity, Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+export enum UserType {
+    USER = 'USER',
+    ADMIN = 'ADMIN'
+}
+
+@Entity()
+export class Auth extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    uid: string;
+
+    @Column({ unique: true })
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    password: string;
+
+    @Column({ nullable: true })
+    nfcid: string;
+
+    @Column({ type: "enum", enum: UserType })
+    type: keyof typeof UserType;
+
+    @OneToOne(type => Club, club => club.cid, {
+        cascade: false
+    })
+    @JoinColumn()
+    cid: Club
+}
