b814034098b66d32249a697e28e47b608ef2c371
Ko-GyeongTae <kokt0203@naver.com>
2021-12-13 22:47:02 +0900

    FIX - database name

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { Auth } from './entities/auth.entity';
 import { JwtStrategy } from './jwt/jwt.startegy';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Auth])
+  ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
   exports: [AuthService]
