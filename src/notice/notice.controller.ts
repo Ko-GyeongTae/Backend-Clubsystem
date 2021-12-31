cf6e13ebad40c4cb39255da4bc1ce84f8a419123
Ko-GyeongTae <kokt0203@naver.com>
2021-12-31 11:36:54 +0900

    ADD - notice API

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.auth.guard';
+import { NoticeService } from './notice.service';
+import { Request } from 'express';
 
 @Controller('notice')
-export class NoticeController {}
+export class NoticeController {
+    constructor(
+        private noticeService: NoticeService,
+    ) { }
+
+    @Post('/')
+    @UseGuards(JwtAuthGuard)
+    createNotice(
+        @Req() req: Request,
+    ) {
+        return this.noticeService.createNotice(req);
+    }
+
+    @Get('/')
+    getNoticeList() {
+        return this.noticeService.getNoticeList();
+    }
+}
--- src/notice/notice.service.ts
+++ src/notice/notice.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import { Payload } from 'src/auth/jwt/jwt.startegy';
 
 @Injectable()
-export class NoticeService {}
+export class NoticeService {
+    constructor() {}
+
+    async createNotice(req: Request) {
+        const user:Payload = req["user"];
+        if (user.type === "USER") {
+            throw new ForbiddenException();
+        }
+        return "Hello"
+    }
+
+    async getNoticeList() {
+        return "HELLO"
+    }
+}
